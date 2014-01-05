48eb61ec0c59b5740457fa9534a842fdfad04cd1
takyam <takayuki.yamaguch.m@gmail.com>
2014-01-05 23:07:16 +0900

    server/sync.jsのエラーハンドリングを調整

--- server/controller/sync.js
+++ server/controller/sync.js
@@ -11,24 +11,30 @@ module.exports = function (method, type, path, data, ack) {
     case 'collection':
       var args = path.split('/')
         , name = args.shift();
-      if (name) {
-        var model_name = inflection.camelize(inflection.singularize(name))
-          , model = mongoose.model(model_name)
-          , sync_method = type + '_' + method;
-        if (model) {
-          if (typeof model[sync_method] === 'function') {
-            model[sync_method](args, data, ack);
-          } else {
-            console.log('model "' + model_name + '" has not method "' + sync_method + '".');
-            ack('model "' + model_name + '" has not method "' + sync_method + '".');
-          }
-        } else {
-          console.log('Unknown model "' + model_name + '".');
-          ack('Unknown model "' + model_name + '".');
-        }
-      } else {
-        console.log('URL is empty.');
-        ack('URL is empty.');
+      if (!name) {
+        ack('Name is empty.');
+        return;
       }
+
+      var model_name = inflection.camelize(inflection.singularize(name))
+        , model = mongoose.model(model_name)
+        , sync_method = type + '_' + method;
+
+      if (!model) {
+        ack('Unknown model "' + model_name + '".');
+        return;
+      }
+
+      if (!_.isFunction(model[sync_method])) {
+        ack('model "' + model_name + '" has not method "' + sync_method + '".');
+        return;
+      }
+
+      model[sync_method](args, data, ack);
+
+      break;
+    default:
+      ack('Unknown type.');
+      return;
   }
 };
